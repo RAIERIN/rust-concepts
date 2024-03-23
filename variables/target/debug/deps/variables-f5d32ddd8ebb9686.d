0c3b11fd4bd1d672b0c920cb6cca8bb6aff696be
RAIERIN <erin@ottr.com.np>
2024-03-23 15:51:34 +0545

    🐛 fix: added git ignore for variables

--- variables/target/debug/deps/variables-f5d32ddd8ebb9686.d
+++
@@ -1,5 +0,0 @@
-/Users/ottrtechnology/Projects/rust/rust-concepts/variables/target/debug/deps/variables-f5d32ddd8ebb9686: src/main.rs
-
-/Users/ottrtechnology/Projects/rust/rust-concepts/variables/target/debug/deps/variables-f5d32ddd8ebb9686.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/ottrtechnology/Projects/rust/rust-concepts/variables/target/debug/variables: /Users/ottrtechnology/Projects/rust/rust-concepts/variables/src/main.rs
