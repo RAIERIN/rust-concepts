0539290da7a8eb4fdb1411438d30f80ba03b0d6b
RAIERIN <erin@ottr.com.np>
2024-03-23 11:41:54 +0545

    initialized project

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,10 @@
+/**
+ * CONCEPTS:
+ * 1. main function is special: it is always the first code that runs in every executable Rust Program, has no params and returs nothing
+ * 2. Function body is wrapped in {}
+ * 3. println! calls a Rust macro (!) means macro
+ * 4. end line with semicolon (;)
+ **/
+fn main() {
+    println!("Hello, world!");
+}
