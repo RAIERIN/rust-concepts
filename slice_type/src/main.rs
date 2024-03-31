e1524b864d375f3b8d9e60cce1f2fa54f97e12e0
RAIERIN <erin@ottr.com.np>
2024-03-31 13:20:23 +0545

    slice type

---
+++ slice_type/src/main.rs
@@ -0,0 +1,35 @@
+/**
+ * Slices let  you reference a contiguous sequence of elements in a collection rather than the whole collection
+ * A slice is a kind of reference, so it does not have ownership
+ */
+
+fn main() {
+    let mut s = String::from("hello world");
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
